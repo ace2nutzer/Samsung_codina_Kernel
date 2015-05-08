b7f5781a5f42638436a9b8dceb98c60c635b29b9
ace2nutzer <ace2nutzer@xda-developers.com>
2015-05-08 22:28:02 +0200

    dbx500-cpufreq: Increase cpuinfo.transition.delay .. it cause probably too much overhead...

--- drivers/cpufreq/dbx500-cpufreq.c
+++ drivers/cpufreq/dbx500-cpufreq.c
@@ -122,7 +122,7 @@ static int __cpuinit dbx500_cpufreq_init(struct cpufreq_policy *policy)
 	 *	   list.
 	 */
 
-	policy->cpuinfo.transition_latency = 5 * 1000; /* in ns */
+	policy->cpuinfo.transition_latency = 10 * 1000; /* in ns */
 
 	/* policy sharing between dual CPUs */
 	cpumask_copy(policy->cpus, &cpu_present_map);
