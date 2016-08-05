7f432f50ba9c7b7c86b1317e43d5009b5570fb87
ace2nutzer <djsilvio1984@gmail.com>
2016-08-05 20:06:06 +0200

    LiveOPP: add more steps

--- drivers/mfd/db8500-prcmu.c
+++ drivers/mfd/db8500-prcmu.c
@@ -1119,7 +1119,10 @@ static struct liveopp_arm_table liveopp_arm[] = {
 	{ 800000,  798720,  0x00050168,   0x25,  0xDB,  100,  100},
 	{1000000,  998400,  0x00050182,   0x32,  0xDB,  100,  100},
 	{1100000,  1098240, 0x0005018F,   0x37,  0x8F,  100,  100},
+	{1150000,  1152000, 0x00050196,   0x37,  0x8F,  100,  100},
 	{1200000,  1198080, 0x0005019C,   0x37,  0x8F,  100,  100},
+	{1250000,  1251840, 0x000501A3,   0x37,  0x8F,  100,  100},
+	{1300000,  1297920, 0x000501A9,   0x37,  0x8F,  100,  100},
 };
 
 static const char *armopp_name[] = 
@@ -1383,6 +1386,9 @@ ARM_STEP(arm_step03, 3);
 ARM_STEP(arm_step04, 4);
 ARM_STEP(arm_step05, 5);
 ARM_STEP(arm_step06, 6);
+ARM_STEP(arm_step07, 7);
+ARM_STEP(arm_step08, 8);
+ARM_STEP(arm_step09, 9);
 
 #if CONFIG_LIVEOPP_DEBUG > 1
 static ssize_t liveopp_start_show(struct kobject *kobj, struct kobj_attribute *attr, char *buf)		
@@ -1413,6 +1419,9 @@ static struct attribute *liveopp_attrs[] = {
 	&arm_step04_interface.attr,
 	&arm_step05_interface.attr,
 	&arm_step06_interface.attr,
+	&arm_step07_interface.attr,
+	&arm_step08_interface.attr,
+	&arm_step09_interface.attr,
 	NULL,
 };
 
