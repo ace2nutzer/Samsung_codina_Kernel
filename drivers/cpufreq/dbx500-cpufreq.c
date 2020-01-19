07a0bb3ab6609ef135d151425a5eb4bee013bc11
ace2nutzer <ace2nutzer@gmail.com>
2020-01-19 08:22:31 +0100

    boot with 200 - 1000 MHz

--- drivers/cpufreq/dbx500-cpufreq.c
+++ drivers/cpufreq/dbx500-cpufreq.c
@@ -99,7 +99,7 @@ static int __cpuinit dbx500_cpufreq_init(struct cpufreq_policy *policy)
 
 	#ifdef CONFIG_DB8500_LIVEOPP
 	policy->min = 200  * 1000;
-	policy->max = 800 * 1000;
+	policy->max = 1000 * 1000;
 	#else
 	policy->min = policy->cpuinfo.min_freq;
 	policy->max = policy->cpuinfo.max_freq;
