2eeae0e183c095d5b77de82000aefea32a9ea5a9
ace2nutzer <ace2nutzer@gmail.com>
2021-02-28 04:28:05 +0100

    zRam, zsmalloc and lz4: update from LK 3.10.103 to 3.18.140

--- kernel/cpu.c
+++ kernel/cpu.c
@@ -29,11 +29,13 @@ void cpu_maps_update_begin(void)
 {
 	mutex_lock(&cpu_add_remove_lock);
 }
+EXPORT_SYMBOL(cpu_maps_update_begin);
 
 void cpu_maps_update_done(void)
 {
 	mutex_unlock(&cpu_add_remove_lock);
 }
+EXPORT_SYMBOL(cpu_maps_update_done);
 
 static RAW_NOTIFIER_HEAD(cpu_chain);
 
@@ -138,6 +140,13 @@ int __ref register_cpu_notifier(struct notifier_block *nb)
 	cpu_maps_update_done();
 	return ret;
 }
+EXPORT_SYMBOL(register_cpu_notifier);
+
+int __ref __register_cpu_notifier(struct notifier_block *nb)
+{
+	return raw_notifier_chain_register(&cpu_chain, nb);
+}
+EXPORT_SYMBOL(__register_cpu_notifier);
 
 static int __cpu_notify(unsigned long val, void *v, int nr_to_call,
 			int *nr_calls)
@@ -161,7 +170,6 @@ static void cpu_notify_nofail(unsigned long val, void *v)
 {
 	BUG_ON(cpu_notify(val, v));
 }
-EXPORT_SYMBOL(register_cpu_notifier);
 
 void __ref unregister_cpu_notifier(struct notifier_block *nb)
 {
@@ -170,6 +178,12 @@ void __ref unregister_cpu_notifier(struct notifier_block *nb)
 	cpu_maps_update_done();
 }
 EXPORT_SYMBOL(unregister_cpu_notifier);
+
+void __ref __unregister_cpu_notifier(struct notifier_block *nb)
+{
+	raw_notifier_chain_unregister(&cpu_chain, nb);
+}
+EXPORT_SYMBOL(__unregister_cpu_notifier);
 
 static inline void check_for_tasks(int cpu)
 {
