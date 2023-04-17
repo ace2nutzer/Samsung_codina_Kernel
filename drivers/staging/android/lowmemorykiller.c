de4d9b859925356894a95788aac021ca8e4b84d6
ace2nutzer <ace2nutzer@gmail.com>
2023-04-17 03:40:34 +0200

    Android/LMK: re-add selected_tasksize this is a partial revert of commit c9f66d88f3c157822bcc72d467a83a8a0f329f19

--- drivers/staging/android/lowmemorykiller.c
+++ drivers/staging/android/lowmemorykiller.c
@@ -81,8 +81,8 @@ static int lowmem_shrink(struct shrinker *s, struct shrink_control *sc)
 	int i = 0;
 	int min_score_adj = OOM_SCORE_ADJ_MAX + 1;
 	int minfree = 0;
+	int selected_tasksize = 0;
 	int selected_oom_score_adj = 0;
-	static char adj_lvl = 0;
 	int array_size = ARRAY_SIZE(lowmem_adj);
 	int other_free = global_page_state(NR_FREE_PAGES) - totalreserve_pages;
 	int other_file = global_page_state(NR_FILE_PAGES) -
@@ -95,7 +95,7 @@ static int lowmem_shrink(struct shrinker *s, struct shrink_control *sc)
 	if (lowmem_minfree_size < array_size)
 		array_size = lowmem_minfree_size;
 
-	for (i = array_size - 1 - adj_lvl; i >= 0; i--) {
+	for (i = 0; i < array_size; i++) {
 		minfree = lowmem_minfree[i];
 		if ((other_free < minfree) && (other_file < minfree)) {
 			min_score_adj = lowmem_adj[i];
@@ -157,10 +157,12 @@ static int lowmem_shrink(struct shrinker *s, struct shrink_control *sc)
 		if (selected) {
 			if (oom_score_adj < selected_oom_score_adj)
 				continue;
-			else
-				break;
+			if (oom_score_adj == selected_oom_score_adj &&
+					tasksize <= selected_tasksize)
+				continue;
 		}
 		selected = p;
+		selected_tasksize = tasksize;
 		selected_oom_score_adj = oom_score_adj;
 		lowmem_print(2, "select '%s' (%d), adj %d, size %d, to kill\n",
 			     p->comm, p->pid, oom_score_adj, tasksize);
@@ -168,7 +170,8 @@ static int lowmem_shrink(struct shrinker *s, struct shrink_control *sc)
 	if (selected) {
 		task_lock(selected);
 		send_sig(SIGKILL, selected, 0);
-		task_set_lmk_waiting(selected);
+		if (selected->mm)
+			task_set_lmk_waiting(selected);
 		task_unlock(selected);
 		lowmem_print(1, "Killing '%s' (%d), adj %d,\n" \
 				"   to free %ldkB on behalf of '%s' (%d) because\n" \
@@ -176,24 +179,21 @@ static int lowmem_shrink(struct shrinker *s, struct shrink_control *sc)
 				"   Free memory is %ldkB above reserved\n",
 			     selected->comm, selected->pid,
 			     selected_oom_score_adj,
-			     tasksize * (long)(PAGE_SIZE / 1024),
+			     selected_tasksize * (long)(PAGE_SIZE / 1024),
 			     current->comm, current->pid,
 			     other_file * (long)(PAGE_SIZE / 1024),
 			     minfree * (long)(PAGE_SIZE / 1024),
 			     min_score_adj,
 			     other_free * (long)(PAGE_SIZE / 1024));
 		lowmem_deathpending_timeout = jiffies + HZ;
-		rem -= tasksize;
+		rem -= selected_tasksize;
 		lowmem_lmkcount++;
-		adj_lvl = 0;
 
 		lowmem_print(4, "lowmem_shrink %lu, %x, return %d\n",
 			     sc->nr_to_scan, sc->gfp_mask, rem);
 	} else {
-		if ((array_size -1) > adj_lvl)
-			adj_lvl++;
 		lowmem_print(2, "lowmem_scan: no killable task for oom_score_adj %hd\n",
-			     min_score_adj);
+				min_score_adj);
 	}
 	rcu_read_unlock();
 	return rem;
